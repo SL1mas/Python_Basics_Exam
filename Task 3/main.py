dab8118a93813e964af727ad6d457a276c046abe
SL1mas <sslimass@gmail.com>
2022-11-12 19:37:14 +0200

    Task 3 (takes any digit number, divides into digits and returns their sum)

--- Task 3/main.py
+++ Task 3/main.py
@@ -1,9 +1,14 @@
-# Sukurkite funkciją "addDigits", kuri priims sveiką skaičių nuo 10 iki 999 ir grąžins jo skaitmenų sumą. 
+# Sukurkite funkciją "addDigits", kuri priims sveiką skaičių nuo 10 iki 999 ir grąžins jo skaitmenų sumą.
 
 # Pvz.
 # Jeigu duota 34, funckiją turėtų grąžinti 7.
 # Jeigu duota 999, funckija turėtų grąžinti 27.
 
 
-def addDigits(number):
-  pass
+# def add_digits(number):
+sum_of_digits = 0
+number = int(input("Įveskite skaičių nuo 10 iki 999 "))
+for digit in range(0, len(str(number))):
+    digit = str(number)[digit]
+    sum_of_digits += int(digit)
+print("suma", sum_of_digits)
