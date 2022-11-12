49f99e8423bf5733636c2c97c14c8082697ccf4c
SL1mas <sslimass@gmail.com>
2022-11-12 20:01:58 +0200

    Finished task 3

--- Task 3/main.py
+++ Task 3/main.py
@@ -5,10 +5,18 @@
 # Jeigu duota 999, funckija turėtų grąžinti 27.
 
 
-# def add_digits(number):
-sum_of_digits = 0
-number = int(input("Įveskite skaičių nuo 10 iki 999 "))
-for digit in range(0, len(str(number))):
-    digit = str(number)[digit]
-    sum_of_digits += int(digit)
-print("suma", sum_of_digits)
+try:
+    number = int(input("Įveskite skaičių nuo 10 iki 999 "))
+
+    def add_digits(number):
+        sum_of_digits = 0
+        if len(str(number)) > 1 and len(str(number)) < 4:
+            for digit in range(0, len(str(number))):
+                digit = str(number)[digit]
+                sum_of_digits += int(digit)
+            return print("Įvesto skaičiaus skaitmenų suma:", sum_of_digits)
+        else:
+            print("Įvestas skaičius nepatenka į intervalą nuo 10 iki 999!!!")
+    sum_of_digits = add_digits(number)
+except:
+    print("Programa priima tik sveikus skaičius nuo 10 iki 999!!!")
